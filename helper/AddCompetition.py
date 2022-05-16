b4100dab470fec8878eeceb8fe1f6e5906da3b6e
quangvn2508 <quangvn2508@gmail.com>
2022-05-17 00:36:23 +0100

    helper to add CP activity, update competitions history

---
+++ helper/AddCompetition.py
@@ -0,0 +1,23 @@
+JSON_FILE = 'public/competitions.json'
+
+def readInput(str):
+    print(str, end=': ')
+    return input()
+
+new_contest = {
+    "tag": readInput("contest series"),
+    "name": readInput("contest name"),
+    "link": readInput("contest URL"),
+    "date": f"{readInput('contest year')}-{readInput('contest month')}-{readInput('contest day')}",
+    "ranking": {
+        "me": readInput("my rank"),
+        "total": readInput("total participants")
+    }
+}
+
+import json
+
+data = json.load(open(JSON_FILE))
+data.append(new_contest)
+with open(JSON_FILE, 'w') as jsonfile:
+    json.dump(data, jsonfile)

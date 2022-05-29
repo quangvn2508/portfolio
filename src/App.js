229c038fa1c01e2ac7b221d2dc773bf7f3a1ff42
quangvn2508 <quangvn2508@gmail.com>
2022-05-29 11:53:29 +0100

    remove mouse tracking background (need remake)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from "./header/Header";
 import MainPage from "./mainpage/MainPage";
 import Footer from "./footer/Footer";
-import { useState } from 'react';
 import {
     BrowserRouter as Router,
     Route,
@@ -12,26 +11,9 @@ import Projects from "./projects/Projects";
 import Profile from "./profile/Profile";
 
 function App() {
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
-    const [focus, setFocus] = useState(false);
-    const handleMouseMove = event => {
-        setX(event.clientX);
-        setY(event.clientY);
-    };
-    const handleTouchMove = event => {
-        setX(event.touches[0].clientX);
-        setY(event.touches[0].clientY);
-    }
     return (
         <Router>
-        <div className="position-relative h-100"
-            onMouseMove={handleMouseMove}
-            onMouseLeave={() => {setFocus(false)}}
-            onMouseEnter={() => {setFocus(true)}}
-            onTouchMove={handleTouchMove}
-            onTouchStart={event => {handleTouchMove(event); setFocus(true)}}
-            onTouchEnd={() => {setFocus(false)}}>
+        <div className="position-relative h-100">
             <Header/>
             <Routes>
                 <Route exact path="/" element={<MainPage />} />
@@ -40,11 +22,6 @@ function App() {
                 <Route exact path="/profile" element={<Profile />} />
             </Routes>
             <Footer/>
-            <div className="w-100 h-100 mouse-layer">
-                <div
-                    className={"mouse-bg " + (focus? "focus":"outfocus")}
-                    style={{left: x, top: y}}/>
-            </div>
         </div>
         </Router>
     );
